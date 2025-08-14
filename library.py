796e05f68df3eec32f27c6cabaf3783d94d1f760
ghofrana <ghofraneoubaid@gmail.com>
2025-08-14 12:40:10 +0100

    Create library.py

---
+++ library.py
@@ -0,0 +1,16 @@
+print("Welcome to the Small Library System!")
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print("Hello, "+first_name+" "+last_name+" HEre are the books available in our library: ")
+print("Availabla Books: ")
+books={1 :"chicka chika boom boom",
+       2: "Brown Bear, what do you see?"}
+for i, j in books.items():
+    print(str(i)+" : "+j)
+x=input("enter the book ID to get details: ")
+information={1:{"titre": "chicka chika boom boom",
+                "autor":"bill martin jr. and jor=hn archambault",
+                "description":"a fun and rhythmic alphabet book.. "}}
+for k in information.keys():
+    for y, z in information[k].items():
+        print(y+" : "+j)
