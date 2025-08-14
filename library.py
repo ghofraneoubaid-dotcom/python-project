27ddf9b007f7851af6a3db7e55f56c1f93519d57
ghofrana <ghofraneoubaid@gmail.com>
2025-08-14 18:07:05 +0100

    Update library.py

--- library.py
+++ library.py
@@ -6,11 +6,20 @@ print("Availabla Books: ")
 books={1 :"chicka chika boom boom",
        2: "Brown Bear, what do you see?"}
 for i, j in books.items():
+    """"
+    a loop that print the book and the ID 
+    """
     print(str(i)+" : "+j)
 x=input("enter the book ID to get details: ")
 information={1:{"titre": "chicka chika boom boom",
                 "autor":"bill martin jr. and jor=hn archambault",
                 "description":"a fun and rhythmic alphabet book.. "}}
 for k in information.keys():
+    """
+    to find the ID choose
+    """
     for y, z in information[k].items():
+        """
+        print the book chose informations
+        """
         print(y+" : "+j)
