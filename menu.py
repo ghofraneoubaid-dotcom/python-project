5f6d7209e825fa88f0989501f6572ca352aa999e
ghofrana <ghofraneoubaid@gmail.com>
2025-08-18 14:02:46 +0100

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,5 @@
+menu = {"pizza": {"100":{"pizza pargherita":5 },
+                "101":{"pizza tuna":6},
+                "102":{"pizza mushroom":7}},
+       "casseroles" : {"200":{"pasta casserole":8 },
+                       "201":{"potato casserole":9}}}
